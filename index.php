19c7f4a3658d7e2575eba7531e85d2c828e79ece
Flávio Augusto Marques Adão <flavioa@liae.edu.uniube.br>
2024-04-25 19:13:04 -0300

    ajustando index

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
-<?php include 'conexao.php'; 
-
-// Buscar filmes já cadastrados
-$sql = "SELECT f.filme,f.titulo,f.ano,g.descricao as genero, g.genero as idGenero FROM filmes f
-inner join generos g on (g.genero=f.genero)";
-$result = $conn->query($sql);
-?>
 <html>
     <head>    
         <title>Cadastro de Filmes</title>
-        <script>
-            <?php 
-            if(isset($_GET['resposta']) && $_GET['resposta']==1){
-                ?>alert("incluido com sucesso");<?php
-            }else if(isset($_GET['resposta']) && $_GET['resposta']==0){
-                ?>alert("erro ao inserir");<?php
-            }else if(isset($_GET['resposta']) && $_GET['resposta']==3){
-                ?>alert("excluído com sucesso");<?php
-            }else if(isset($_GET['resposta']) && $_GET['resposta']==4){
-                ?>alert("erro ao excluir");<?php
-            } ?>
-        </script>
+        
     </head>
     <body>
         <h1>Cadastro de Filmes</h1>
